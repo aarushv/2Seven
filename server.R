a013249fee4979a73cea0fa823cf4d9a56bc5435
Logan Marek <marekl@uw.edu>
2018-03-04 15:22:51 -0800

    Almost done with all graphs

--- server.R
+++ server.R
@@ -8,7 +8,6 @@ library(plotly)
 #This is for a faster read in of data
 library(data.table)
 
-
 students <- fread("recent-grads.csv", stringsAsFactors = FALSE)
 
 shinyServer(function(input,output){
@@ -18,22 +17,49 @@ shinyServer(function(input,output){
       
       ###major_select is the input value for which major is being selected
       select(Major,Men,Women,Total) %>% 
-      filter(Major == major_select)
-    
-    ##this counts all the men in the specified major
-    just_men <- men_women %>% 
-      select(Men) %>% 
-      summarize(sum(Men))
-    #This counts all the women in the specified major
-    just_women <- men_women %>% 
-      select(Women) %>% 
-      summarize(sum(Women))
+      filter(Major == input$major_select)
     
     #This plots the men vs women in a pie chart
-    plot_ly(data = men_women, labels = c("Men", "Women"), values = ~c(Men,Women), type = 'pie')
+    plot_ly(data = men_women, labels = c("Men", "Women"), values = ~c(Men,Women), type = 'pie') %>% 
+      layout(title = 'Gender Ratio in Major')
+  })
+  ##This outputs the pie chart for employment statistics
+  output$employed <- renderPlot({
     
+    ###major_select is the input value for which major is selected
+    employed <- students %>% 
+      select(Major,Employed,Unemployed,College_jobs,Non_college_jobs,Low_wage_jobs) %>% 
+      filter(Major == input$major_select)
     
+    plot_ly(data = employed, labels = c("Jobs that Require College Degree", "Jobs that Do Not Require a College Degree", "Jobs Close to or at Minimum Wage","Unemployed"), values = ~c(College_jobs,Non_college_jobs, Low_wage_jobs, Unemployed), type = 'pie', marker = list(colors = c("#66a3ff","#4d94ff","#3385ff","#ff3333"))) %>% 
+      layout(title = 'Employment vs Unemployment')
+  })
+  ##This outputs the bar graph for pay 
+  output$pay <- renderPlot({
     
+    ##major_select is the input value for which major is selected
+    pay <- students %>% 
+      select(Major,P25th,Median,P75th) %>% 
+      filter(Major == input$major_select)
     
+    plot_ly(data = pay, x = c("25th Percentile", "Median", "75th Percentile"), y = ~c(P25th, Median, P75th), type = 'bar') %>% 
+      layout(title = 'Pay Rate', yaxis = list(title = "Pay Rate in Thousands of Dollars"))
+  })
+  
+  output$department_major <- renderPlot({
+    pay2 <- students %>% 
+      select(Major,Major_category,Median) %>% 
+      filter(Major == "ECONOMICS")
+      
+   department_pay <- students %>% 
+     select(Major,Major_category,Median) %>% 
+     filter(Major_category == pay2[[2]]) %>% 
+     summarize(Median = mean(Median))
+   
+   Department_Median <- as.data.frame(c("Major" = pay2[[2]],"Major_category" = pay2[[2]],department_pay))
+   
+   pay_comparison <- full_join(pay2,Department_Median)
+   
+   plotly()
   })
 })
