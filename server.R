374b7f3b595b5f2b7084d6229fdbe7be4d0e2c6d
Logan Marek <marekl@uw.edu>
2018-03-04 14:06:00 -0800

    Gender chart done

--- server.R
+++ server.R
@@ -14,6 +14,26 @@ students <- fread("recent-grads.csv", stringsAsFactors = FALSE)
 shinyServer(function(input,output){
   ##This ouputs the first graph: men vs women
   output$men_women <- renderPlot({
+    men_women <- students %>% 
+      
+      ###major_select is the input value for which major is being selected
+      select(Major,Men,Women,Total) %>% 
+      filter(Major == major_select)
+    
+    ##this counts all the men in the specified major
+    just_men <- men_women %>% 
+      select(Men) %>% 
+      summarize(sum(Men))
+    #This counts all the women in the specified major
+    just_women <- men_women %>% 
+      select(Women) %>% 
+      summarize(sum(Women))
+    
+    #This plots the men vs women in a pie chart
+    plot_ly(data = men_women, labels = c("Men", "Women"), values = ~c(Men,Women), type = 'pie')
+    
+    
+    
     
   })
 })
