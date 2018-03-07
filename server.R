809d27b12e18e7a8beb1a462d651aba49ef7061a
aarushv <35243028+aarushv@users.noreply.github.com>
2018-03-07 01:30:37 -0800

    If else added

--- server.R
+++ server.R
@@ -30,11 +30,11 @@ compare.server <- shinyServer(function(input, output) {
   ##Renders plot comparing the median pay of selected majors
   output$pay.comp <- renderPlotly({
 
-    pay.majors1 <- filter_(recent_grads, recent_grads$Major==input$Major1) %>%
+    pay.majors1 <- filter(recent_grads, recent_grads$Major==input$Major1) %>%
       select(Major, Median)
-    pay.majors2 <- filter_(recent_grads, recent_grads$Major==input$Major2) %>%
+    pay.majors2 <- filter(recent_grads, recent_grads$Major==input$Major2) %>%
       select(Major, Median)
-    pay.majors3 <- filter_(recent_grads, recent_grads$Major==input$Major3) %>%
+    pay.majors3 <- filter(recent_grads, recent_grads$Major==input$Major3) %>%
       select(Major, Median)
     
     paycomp.selected <- rbind(pay.majors1, pay.majors2, pay.majors3)
@@ -64,6 +64,17 @@ compare.server <- shinyServer(function(input, output) {
     
   })
   
+  if(input$displayOption == "gender.comp"){
+    plotlyOutput("gender.comp") 
+  } else if(input$displayOption == "emp.comp") {
+    plotlyOutput("emp.comp")
+  } else if (input$displayOption == "pay.comp"){
+    plotlyOutput("pay.comp")
+  } else {
+    plotlyOutput("gender.comp");
+    plotlyOutput("emp.comp");
+    plotlyOutput("pay.comp")
+  }
 })
 
 
